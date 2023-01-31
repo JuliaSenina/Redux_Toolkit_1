f031f42611e6163d0fa2e8543af61144c08b2332
Сенина Юлия Александровна <y.senina@ens.ru>
2023-01-31 12:26:24 +0300

    commit

---
+++ toolkit/src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Evidence from "./components/Evidence";
+
+function App() {
+  return <Evidence />;
+}
+
+export default App;
---
+++ toolkit/src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import evidenceSlice from "../slice/evidenceSlice";
+
+export const store = configureStore({
+  reducer: {
+    evidence: evidenceSlice,
+  },
+});
